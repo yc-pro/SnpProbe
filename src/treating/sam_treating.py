b23c975b3a3a6275257567c71d1c93cc2664f063
yc_i7 <yc_pl@wuchan-yc>
2020-01-17 20:28:25 +0800

    普片

--- src/treating/sam_treating.py
+++ src/treating/sam_treating.py
@@ -65,9 +65,52 @@ def is_only(file_in,file_out):
                     fout.write(key+"\t"+ vec1[0] + "\t" + vec1[1] + "\t"  +vec1[2]+"\t2\t\n")
     return 0
 
-def is_only_ordely(file_in,file_out):
+'''
+开始寻找最小得分，并输出记录
+'''
+def is_minfen(mm):
+    mm_re = {}
+    for key in mm:
+        vec = mm[key][0:-1].split("#")
+        mm1 = {}
+        for ii in range(0,len(vec)):
+            vec1 = vec[ii].split("@")
+            fen = int(vec1[1])
+            if(mm1.has_key(fen)):
+                mm1[fen] += 1   #对比对得分进行统计，如果有多个相同得分，则加1
+            else:
+                mm1.setdefault(fen,1)
+        max_key = max(mm1) #找到最小得分
+        max_value = mm1[max_key]    #得到最小得分比对到referance的位置的信息
+        if(max_value == 1): #最小得分是否只在ref的一个位置出现
+            str_re = ""
+            for ii in range(0,len(vec)):
+                vec1 = vec[ii].split("@")
+                fen = int(vec1[1])
+                if(fen == max_key): #找到最小得分的信息
+                    if(vec1[2] =="0"):  #判断时候是不包含indel的，vec1[0]位点，vec1[1]得分，vec1[2]indel情况
+                        str_re = key+"\t"+ vec1[0] + "\t" + vec1[1] + "\t"  +vec1[2]+"\t1"
+                    else:
+                        str_re = key+"\t"+ vec1[0] + "\t" + vec1[1] + "\t"  +vec1[2]+"\t1" #包含indel
+            mm_re.setdefault(1,str_re)
+                
+        else:
+            itcc = 1
+            #包含多个最小得分
+            for ii in range(0,len(vec)):
+                vec1 = vec[ii].split("@")
+                fen = int(vec1[1])
+                if(fen == max_key):
+                    str_re = key+"\t"+ vec1[0] + "\t" + vec1[1] + "\t"  +vec1[2]+"\t2"
+                    mm_re.setdefault(itcc,str_re)
+                    itcc += 1
+    return mm_re
+
+def is_only_ordely(file_in,file_out,file_out1):
     fout = open(file_out,'w')
+    fout1 = open(file_out1,'w')
     mm = {}
+    mm_true = {} #用于判断，是否reads比对是顺序的
     for line in open(file_in):
         if(line[0] == "@"):
             continue
@@ -85,10 +128,35 @@ def is_only_ordely(file_in,file_out):
             elif(ui.find("XO:i:") >= 0):    #找到indel的情况
                 indel = veckk[2] 
         sk = site+"@"+fen+"@"+ indel
+        
+        #以下是开始进行判断
         if(mm.has_key(id)): #id为reads的编号
             mm[id] += sk +"#"
-        else:
+        elif(len(mm) == 0):
             mm.setdefault(id,sk+"#")
+        else:
             
+            mm_re = is_minfen(mm)   #先把旧信息写入，找到该reads的最小得分，并返回等分信息的map
+            for key in mm_re:
+                fout.write(mm_re[key] + "\t" + str(key) + "\n") #进行文件写入
+                
+            old_id = min(mm)
+            if(mm_true.has_key(old_id)):
+                mm_true[old_id] += 1
+            else:
+                mm_true.setdefault(old_id,1)
+                
+            mm.clear()
+            mm.setdefault(id,sk+"#") #清空map后再写入新信息
+    
+    #写人最后一个reads的记录
+    mm_re = is_minfen(mm)
+    for key in mm_re:
+        fout.write(mm_re[key] + "\t" + str(key) + "\n")    
+        
+    for key in mm_true:
+        fout1.write(str(key) + "\t" + str(mm_true[key]) + "\n")
+    return 0
+
 if __name__ == '__main__':
     print("hello word")
